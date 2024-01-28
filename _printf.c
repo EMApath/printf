8078d719c3f3aaf6271fc1910ab74aa1598056a8
Eman Bashir <eman_bashir@outlook.com>
2024-01-28 21:56:32 +0300

    update function documentation

--- _printf.c
+++ _printf.c
@@ -1,43 +1,17 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _printf- prints out a character into std output
  *
- * @format: charracter to be printed
+ * @format: format string with string literals and specifiers
+ * @args: passed arguments to be ccnverted into their values
+ * 
  * Return: number of charachters printed
  */
 
 int _printf(const char *format, ...)
 {
-	int i;
-	char var_char;
-	char *var_string;
-	va_list list;	
-	va_start(list, format);
 	
-	char *chars = format;
-
-  	for (i = 0; *chars != '\0'; chars++)
-	{
-		if (*chars != '%')
-		{
-			_putchar(*chars);
-		}
-		else
-		{
-			chars++;
-			printf("222");
-			if (*chars == "c")
-				var_char = va_arg(format ,char);
-			
-			else if (*(chars) == "s")
-			{
-				printf("111");
-				*var_string = va_arg(format ,char *);
-				for (i = 0; *var_string != '\0'; var_string++)
-					_putchar(*var_string);
-			}
-		}
-	}
-	va_end(list);
 }
