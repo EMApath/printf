828976e74a007531115b0fa13aa553693faad340
Eman Bashir <eman_bashir@outlook.com>
2024-01-28 23:11:32 +0300

    styling modifications

--- _printf.c
+++ _printf.c
@@ -6,21 +6,21 @@
  * _printf- prints out a character into std output
  *
  * @format: format string with string literals and specifiers
- * @args: passed arguments to be ccnverted into their values
- * 
+ *
  * Return: number of charachters printed
  */
 
 int _printf(const char *format, ...)
 {
-	
 	int i;
 	int printed_num = 0;
-	va_list list;	
+
+	va_list list;
 	va_start(list, format);
+	
 	char *chars = format;
 
-  	for (i = 0; *chars != '\0'; chars++)
+	for (i = 0; *chars != '\0'; chars++)
 	{
 		if (*chars != '%')
 		{
@@ -32,14 +32,16 @@ int _printf(const char *format, ...)
 			chars++;
 			if (*chars == 'c')
 			{
-				char var_char = va_arg(list ,char);
+				char var_char = va_arg(list, char);
+
 				_putchar(var_char);
 				printed_num++;
 			}
 
 			if (*chars == 's')
 			{
-				char *var_string = va_arg(list ,char *);
+				char *var_string = va_arg(list, char *);
+				
 				for (i = 0; *var_string != '\0'; var_string++)
 				{
 					_putchar(*var_string);
