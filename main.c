cfe43bb899c01ea4b4df515cd6d5206e46698c7f
Eman Bashir <eman_bashir@outlook.com>
2024-01-28 22:04:21 +0300

    update function documentation

--- main.c
+++ main.c
@@ -9,9 +9,15 @@
  */
 int main(void)
 {
-      char *len = "1000";
+     /* char *len = "1000";
 
       _printf("Length:[%s]sssss\n",len);
-      printf("Length:[%s]sssssss\n",len);
+      printf("Length:[%s]sssssss\n",len);*/
+	_printf("Hello world\n");
+	printf("Hello world\n");
+	_printf("charecters: %c %c\n", 'a', 65);
+	printf("charecters: %c %c\n", 'a', 65);
+	_printf('this is a percent sign %% \n');
+	printf('this is a percent sign %% \n');
       return (0);
 }
